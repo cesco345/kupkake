85e75c2d3291d2c83e4ce8a1324bcb89510149bc
Francesco Piscani <fpiscani@gmail.com>
2024-03-30 15:45:49 -0400

    Static Site Generator

---
+++ src/app/[slug].tsx
@@ -0,0 +1,43 @@
+import { View, Text, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { useLocalSearchParams } from "expo-router";
+import { getPost, getAllPosts } from "../repository/postRepository";
+import Markdown from "react-native-markdown-display";
+
+export async function generateStaticParams(): Promise<
+  Record<string, string>[]
+> {
+  const posts = getAllPosts();
+  // Return an array of params to generate static HTML files for.
+  // Each entry in the array will be a new page.
+  return posts.map((post) => ({ slug: post.slug }));
+}
+
+const PostDetailsPage = () => {
+  const { slug } = useLocalSearchParams();
+  const [post, setPost] = useState(getPost(slug));
+
+  if (!post) {
+    return <Text>Post not found</Text>;
+  }
+  return (
+    <ScrollView
+      style={{
+        flex: 1,
+        backgroundColor: "white",
+      }}
+      contentContainerStyle={{
+        gap: 24,
+        padding: 24,
+        width: "100%",
+        maxWidth: 960,
+        marginHorizontal: "auto",
+      }}
+    >
+      <Text style={{ fontSize: 30, marginBottom: 20 }}>{post.title}</Text>
+      <Markdown>{post.content}</Markdown>
+    </ScrollView>
+  );
+};
+
+export default PostDetailsPage;
---
+++ src/content/post1.ts
@@ -0,0 +1,69 @@
+import { Post } from '../types/post';
+
+const post: Post = {
+  slug: 'post1',
+  date: '2024-02-28T13:27:28Z',
+  title: 'My First Post',
+  description: 'This is the first post in my app.',
+  thumbnail: '',
+  content: `# Parte carissime reddit
+
+  ## Illa mundi tamen aut hoc sinistra passi
+  
+  **Lorem markdownum** ferre, curia torpetis cultores auro Nile sub, haec. Sunt
+  ponto [aestus](http://obstas.io/morisqui) ex laniger Pallada quantus taedia
+  longique laniabat accessit poenaeque regem certa belli? Secretos vulnera: genis
+  non prole induxerat aquas mensae haud reiecit posito. Loca ferae, subvectaque
+  tempora? Dique et vult?
+  
+  > Non se ego ignorans arbor tecum *biformis* de Aonides eadem. Volventem virgo
+  > dominus aedificat flava praepositam habet, *ortum praebetis*? Sinamus merumque
+  > fratres, conscendunt monstris domino ulla puellam; Lyciae quia Caicus nitidi;
+  > nostras poteram parantur. Labens effigiem versa unam latices!
+  
+  ## Lacusque omnia videnda
+  
+  Aut rediret fiet. [Miserere](http://solebantposse.org/abit-versat.aspx) undas
+  manet: aqua litore suis populus adsumit spatium faveat percutimurque erat
+  Iuppiter. Eandem notam adsimulavit tamen: cui **mitte resolvo** posse haud
+  ferox, arbore factaque si **est**. Querellas caeso. Tanta inter Iunone subito,
+  funera superorum mediis.
+  
+  ## Turbatum cultus exercita corpus molitur dabo levati
+  
+  Peti ad lecto senectus liceat [per](http://resecare.io/longaevadominique.html).
+  [Nostri ripas](http://utqueonerosior.net/iniuria-demisit) et populus sed egere
+  tinnitibus circumdata altera et si nihil **me concipit Ecce**: augur
+  [est](http://prensamquemisit.org/haerentem)? Exarsit perluit sepulcro Caicus
+  atque; numquam, facies *callidus*, neu agit. Sorte armenta Tyrios concreta
+  stella vidi referebat quaque: rostro: quos: unde morte ortus quaerenti o pudor
+  dubiae.
+  
+  ## Infesto per sibi confine sine est
+  
+  Studiosus coniuge maiora movit posse noctes Circen filis clipeo **Ut** securior
+  barbara solent genitis? Annos est hanc.
+  
+  ## Solus referam
+  
+  Mortalem Iovis dea sanguine reperta persequitur donec marmoreoque quod susurra
+  nostro accedere quaecumque summo! Riguo haec equique at nulla ora pede
+  pulcherrime aqua; *orbe furit* mallem salve foret origine pars, vix. Cacumen se
+  sub tua manus undarum non leucada natorum est, cura qua Panaque ullus, o chordas
+  meae. Aesculea inridet postquam et cucurri videat ad medio mater restituit, vos
+  quod luet mihi signa, leones, faveas. Movi gentes exquirere malae sollertius
+  Titanida, et gaudebat **tollere**.
+  
+  Ingentia virginis quod invita ramos per audita cristati labores dixit sim,
+  Aesoniden. Qui puerosque sororis dextra ullis corpore summa servaturis memorabat
+  subito praedae minuunt! Misit coniugium in vincis iussis, animae gramine quam
+  scinditque circum, rostrum erat Hippodamen risitque.
+  
+  Terrae Alpes. Valles favet; anno posset vana quam longum, ubi quoque mea
+  moventem, at. Mota formam flammis concretaque tempora timuitque certaminis cavas
+  aequoris exigis montis trepident nusquam
+  [longis](http://www.serpentem-dicitur.net/in). Bello plus dixit conantur a nos
+  reddat det, fovesque in exilibus ligno totaque fabrae reddi.`,
+};
+
+export default post;

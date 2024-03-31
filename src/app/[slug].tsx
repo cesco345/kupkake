e37241fa2e3fe44895394247a730682a46f202da
Francesco Piscani <fpiscani@gmail.com>
2024-03-30 20:39:01 -0400

    added thumbnails to posts

--- src/app/[slug].tsx
+++ src/app/[slug].tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import React, { useState } from "react";
 import { useLocalSearchParams } from "expo-router";
 import { getPost, getAllPosts } from "../repository/postRepository";
@@ -41,6 +41,11 @@ const PostDetailsPage = () => {
         }}
       >
         <Text style={{ fontSize: 30, marginBottom: 20 }}>{post.title}</Text>
+        <Image
+          source={{ uri: `/thumbnails/${post.thumbnail}` }}
+          style={{ width: "100%", aspectRatio: 16 / 9 }}
+          alt={post.title}
+        />
         <Markdown>{post.content}</Markdown>
       </ScrollView>
     </>
